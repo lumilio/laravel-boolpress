34b3243bb6fcba04fbe0950a5d289d6703b75cf9
lumilio <logistic-division@lumilium.com>
2022-01-27 16:45:24 +0100

    Update Logaout in admin.blade

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,8 +19,7 @@ mix
     .copyDirectory('resources/img', 'public/img')
 
     .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-
     .js('resources/js/admin.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/admin.scss', 'public/css');
 
