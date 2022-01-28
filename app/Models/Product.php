8c8267a4c5fbb1e3039d1948a66f99560641a655
lumilio <logistic-division@lumilium.com>
2022-01-28 21:48:45 +0100

    Ultimate crate and store routes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [            
+    'name',
+    'image',
+    'price',
+    'quantity',
+    'description',];
 }
