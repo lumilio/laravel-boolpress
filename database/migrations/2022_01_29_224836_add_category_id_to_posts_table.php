662e359608ccec84e671f92b7b5af19d7c78a984
lumilio <logistic-division@lumilium.com>
2022-01-30 00:18:17 +0100

    #

--- database/migrations/2022_01_29_224836_add_category_id_to_posts_table.php
+++ database/migrations/2022_01_29_224836_add_category_id_to_posts_table.php
@@ -14,7 +14,7 @@ class AddCategoryIdToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
         });
     }
