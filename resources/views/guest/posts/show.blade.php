9507d0e8004d6fb8a851634cff16582f8ed6ead4
lumilio <logistic-division@lumilium.com>
2022-01-30 15:28:39 +0100

    Update form category + dom

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -7,6 +7,7 @@
             <img class='mb-2'src="{{$post->cover}}" alt="">
             <h4>post scritto da utente n.{{$post->id}}</h4>
             <p>{{$post->description}}</p>
+            <p>categoria : {{$post->category != null ? $post->category->name : 'non specificata'}}</p>
             @auth
             <a href="{{route('admin.posts.edit', $post->slug)}}">modifica</a>
             @endauth
