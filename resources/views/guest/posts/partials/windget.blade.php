dd9d3ff53cf4b5a21d5acdbbfca679fba181bb1d
lumilio <logistic-division@lumilium.com>
2022-01-31 12:10:48 +0100

    Brainstorming rotte con slug #

--- resources/views/guest/posts/partials/windget.blade.php
+++ resources/views/guest/posts/partials/windget.blade.php
@@ -1,13 +1,32 @@
-<div style='width:300px; height:100%; position: sticky; top: 10px; 'class="container is-fixed-top text-white bg-secondary p-3">
-    <ul style='list-style:none;'>
-        <li><a style='color:white;' href="{{route('guest.posts.index')}}">All</a></li>
-        @forelse ($category_arrey as $item)
-        <li>
-            <a style='color:white;' href="{{route('guest.categories.post', $item->slug)}}">{{$item->name}}</a>
-            <span class="badge bg-primary">{{$item->posts()->count()}}</span>
-        </li>
-        @empty
-        <li>no data</li>
-        @endforelse 
-    </ul>
+<div class="d-flex flex-column">
+    <div style='width:300px;  position: sticky; top: 10px; 'class="container is-fixed-top text-white bg-secondary p-3">
+        <ul style='list-style:none;'>
+            <h3>Categories</h3>
+            <li><a style='color:white;' href="{{route('guest.posts.index')}}">All</a></li>
+            @forelse ($category_arrey as $item)
+            <li>
+                <a style='color:white;' href="{{route('guest.categories.post', $item->slug)}}">{{$item->name}}</a>
+                <span class="badge bg-primary">{{$item->posts()->count()}}</span>
+            </li>
+            @empty
+            <li>no data</li>
+            @endforelse 
+        </ul>
+
+        <ul class='mt-5' style='list-style:none;'>
+        <h3>Tags</h3>
+            <li><a style='color:white;' href="{{route('guest.posts.index')}}">All</a></li>
+            @forelse ($category_arrey as $item)
+            <li>
+                <a style='color:white;' href="{{route('guest.tags.post', $item->slug)}}">{{$item->name}}</a>
+                <span class="badge bg-primary">{{$item->posts()->count()}}</span>
+            </li>
+            @empty
+            <li>no data</li>
+            @endforelse 
+        </ul>
+    </div>
 </div>
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::namespace('Guest')->prefix('guest')->name('guest.')->group(function(){
     Route::resource('posts','PostController')->only(['index','show'])->parameter('post', 'post:slug');
     Route::get('categories/{category:slug}/posts', 'CategoryController@index')->name('categories.post');
     Route::get('tags/{tags:slug}/posts', 'TagController@index')->name('tags.post');
-
 });
 
 Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
@@ -32,9 +31,10 @@ Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('auth')->
     Route::resource('products','ProductController')->parameter('product', 'post:slug');
     Route::resource('posts','PostController')->parameter('product', 'post:slug');
     Route::resource('categories','CategoryController')->parameter('product', 'post:slug');
+    Route::resource('tags','TagController')->parameter('product', 'post:slug');
 });
 
 
-
+/* ------ slug ?? ------- */
 
 
