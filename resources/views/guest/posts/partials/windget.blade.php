fb147af88cc2f26ce86cadd40568895bc729f27b
lumilio <logistic-division@lumilium.com>
2022-01-31 02:14:37 +0100

    #

--- resources/views/guest/posts/partials/windget.blade.php
+++ resources/views/guest/posts/partials/windget.blade.php
@@ -1,6 +1,6 @@
 <div style='width:300px; height:100%; position: sticky; top: 10px; 'class="container is-fixed-top text-white bg-secondary p-3">
     <ul style='list-style:none;'>
-        <li><a style='color:white;' href="">All</a></li>
+        <li><a style='color:white;' href="{{route('guest.posts.index')}}">All</a></li>
         @forelse ($category_arrey as $item)
         <li><a style='color:white;' href="{{route('guest.categories.post', $item->slug)}}">{{$item->name}}</a></li>
         @empty
