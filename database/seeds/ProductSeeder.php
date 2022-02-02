baadb06d53a7b356894b51eece6510c6ec2d0680
lumilio <logistic-division@lumilium.com>
2022-02-02 04:42:30 +0100

    Fix faker error

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -17,7 +17,7 @@ class ProductSeeder extends Seeder
         for ($i=0; $i < 10; $i++) { 
             $prod = new Product();
             $prod->name = $faker->sentence();
-            $prod->image = 'faker_store_images/'. $faker->image('public/storage/faker_store_images',600, 400, 'Products',true);
+            $prod->image = 'faker_store_images/'. $faker->image('storage/app/public/faker_store_images',600, 400, 'Products',false,true, $prod->name);
             $prod->price = $faker->randomFloat(2, 100, 300);
             $prod->quantity = $faker->numberBetween(1, 100);
             $prod->description = $faker->text();
